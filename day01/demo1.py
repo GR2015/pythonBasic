9321b6621112262b653209d4c9a107c2b0ea98ec
qinyong <327438521@qq.com>
2015-08-15 13:41:48 +0800

    ewe

---
+++ day01/demo1.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+a=100
+while (a>60):
+  a=int(raw_input("啊啊老调侃别人："))
+  if a<10:
+     print "是笨蛋"
+  elif (a>10) and(a<20):
+       print "是个二货"
+  else:
+        print "是个屌丝"
